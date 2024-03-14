75821e95ceeba29cefda0c73eeb217d7a0ee0532
Patryk Olechno <panoptikum28@gmail.com>
2024-03-14 12:16:16 +0100

    feat: [task-1] Message1

---
+++ Bird.java
@@ -0,0 +1,12 @@
+public class Bird {
+    public static void main(String[] args) {
+        System.out.println("Bird");
+
+        String s = "Stork";
+        String p = "Pstrąg";
+        int i = 10;
+        int b = 20;
+        int c = 30;
+        int d = 40;
+    }
+}
